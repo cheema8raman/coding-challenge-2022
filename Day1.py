2fd30c85c33352a59034ecca009db470d6e80449
Ramanpreet Kaur <cheema8raman@gmail.com>
2022-01-01 07:41:37 +0100

    Update Day1.py

--- Day1.py
+++ Day1.py
@@ -6,7 +6,7 @@ Given a list of numbers and a number k, return whether any two numbers from the 
 For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
 '''
 
-# Way 1
+
 def is_sum_to(a, k):
 
     '''
